b6ab728df8c24ae58110399ed11a54d3b6a20dcb
Samer Afach <info@afach.de>
2022-11-01 19:13:38 +0400

    Add check for Send trait for Database

--- src/database.rs
+++ src/database.rs
@@ -51,3 +51,13 @@ impl Database {
         self.dbi
     }
 }
+
+mod test {
+    use super::*;
+    // Just to check this compiles
+    #[allow(unused)]
+    fn database_is_send_sync(db: Database) {
+        fn is_send_sync(_x: impl Send + Sync) {}
+        is_send_sync(db)
+    }
+}
